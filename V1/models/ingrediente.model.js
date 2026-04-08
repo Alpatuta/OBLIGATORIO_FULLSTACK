79055430d20b84f14a700f34ace9a05ec65b3fa3
foteiza <fede.oteiza@gmail.com>
2026-04-08 09:26:06 -0300

    Agregar modelo de ingrediente con esquema y exportación

---
+++ V1/models/ingrediente.model.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const ingredienteSchema = new mongoose.Schema({
+    nombre: { type: String, required: true },
+    cantidad: { type: Number, required: true },
+    unidad: { type: String, required: true },
+    categoria: { type: String, required: true },
+});
+
+export default mongoose.model("Ingrediente", ingredienteSchema);
+/**Esto hace que el primer ingrediente se guarda en la coleccion ingredientes. La generación de la colección es automática según el nombre del modelo. Lo hace en ingles por lo que cuando creo un modelo que solo con una s al final no hace plural, lo agrego como tercer parámetro */
