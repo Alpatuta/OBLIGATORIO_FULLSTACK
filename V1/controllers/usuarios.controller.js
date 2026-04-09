1ed19d1ef68bb171f645d6a4b51cdbfdf8215d74
Magodnes <ropin1697@gmail.com>
2026-04-09 14:31:49 -0300

    En usuarios.Services creo const para cambiarplan de usuario. A su vez, en usuarios controller, construyo logica para manejo de excepciones y mostrado de mensajes exito y error al cambiar plan.

--- V1/controllers/usuarios.controller.js
+++ V1/controllers/usuarios.controller.js
@@ -1,6 +1,24 @@
 import jwt from "jsonwebtoken";
+import { cambiarPlanUsuarioService } from "../services/usuarios.services";
 
-export const crearUsuario= (req, res) => {
 
-    
-}
+export const cambiarPlanUsuario = async (req, res) => {
+
+    try {
+        const username = req.body.nombre;
+        const usuarioActualizado = await cambiarPlanUsuarioService(username);
+        res.status(200).json({ message: "Plan cambiado a premium exitosamente", usuario: usuarioActualizado });
+    } catch (error) {
+        if (error.message === "Usuario no encontrado") {
+            return res.status(404).json({ message: error.message });
+        }
+        if (error.message === "El usuario ya tiene el plan premium") {
+            return res.status(400).json({ message: error.message });
+        }
+        res.status(500).json({ message: "Error al cambiar el plan del usuario", error: error.message });
+    }
+
+
+};
+
+
--- V1/services/usuarios.services.js
+++ V1/services/usuarios.services.js
@@ -1,7 +1,23 @@
 import Usuario from "../models/usuario.model.js";
 
+
 export const registrarUsuarioService = async (nuevoUsuario) => {
     const usuarioCreado = new Usuario(nuevoUsuario);
     await usuarioCreado.save();
     return usuarioCreado;
+}
+
+export const cambiarPlanUsuarioService = async (nombre) => {
+    const usuario = await Usuario.findOne({ nombre });
+
+    if (!usuario) {
+        throw new Error("Usuario no encontrado");
+    }  
+    if (usuario.plan === "premium") {
+        throw new Error("El usuario ya tiene el plan premium");
+    }
+
+    usuario.plan = "premium";
+    await usuario.save();
+    return usuario;
 }
