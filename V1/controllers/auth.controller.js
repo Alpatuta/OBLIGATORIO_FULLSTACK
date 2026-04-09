20a2e8961c50a8b0d38f65c6a1d7ac4998eab6ea
foteiza <fede.oteiza@gmail.com>
2026-04-09 12:50:10 -0300

    Agregando servicio usuario y cambiando la logica en el auth controller, en la función resgisterUsuario

--- V1/controllers/auth.controller.js
+++ V1/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 import Usuario from "../models/usuario.model.js";
+import { registrarUsuarioService } from "../services/usuarios.services.js";
 
 //LOGIN
 
@@ -24,8 +25,8 @@ export const loginUsuario = async (req, res) => {
 
     //Si la autenticacion es correcta, genero un token con la información del usuario y lo devuelvo al cliente.
     const token = jwt.sign({ id: usuarioLogueado._id, nombre: usuarioLogueado.nombre, plan: usuarioLogueado.plan },
-         process.env.SECRET_JWT, { expiresIn: "1h" });
-        res.status(200).json({ message: "Login exitoso", token });
+        process.env.SECRET_JWT, { expiresIn: "1h" });
+    res.status(200).json({ message: "Login exitoso", token });
 }
 
 //logica de registro de usuario
@@ -34,23 +35,23 @@ export const registerUsuario = async (req, res) => {
 
     const { nombre, contrasenia, plan, confirmarContrasenia } = req.body;
 
-    if(contrasenia!== confirmarContrasenia){
+    if (contrasenia !== confirmarContrasenia) {
         return res.status(400).json({ message: "Las contraseñas no coinciden" });
     }
 
     const hashedPassword = bcrypt.hashSync(contrasenia, process.env.ROUNDS);
 
-    const usuarioExistente = await Usuario.findOne ({ nombre });
+    const usuarioExistente = await Usuario.findOne({ nombre });
 
     if (usuarioExistente) {
         return res.status(400).json({ message: "El nombre de usuario ya esta en uso." });
     }
 
-    let nuevoUsuario = {nombre: req.body.nombre, contrasenia: hashedPassword, plan: req.body.plan};
+    let nuevoUsuario = { nombre: req.body.nombre, contrasenia: hashedPassword, plan: req.body.plan };
 
-    await nuevoUsuario.save(); //guardo el nuevo usuario en la base de datos
+    const usuarioCreado = await registrarUsuarioService(nuevoUsuario);
 
-    const TOKEN = jwt.sign({nombre: nuevoUsuario.nombre, plan: nuevoUsuario.plan}, process.env.SECRET_JWT, { expiresIn: "1h" });
+    const TOKEN = jwt.sign({ nombre: usuarioCreado.nombre, plan: usuarioCreado.plan }, process.env.SECRET_JWT, { expiresIn: "1h" });
 
     res.status(201).json({ message: "Usuario registrado exitosamente", token: TOKEN });
 }
---
+++ V1/services/usuarios.services.js
@@ -0,0 +1,7 @@
+import Usuario from "../models/usuario.model.js";
+
+export const registrarUsuarioService = async (nuevoUsuario) => {
+    const usuarioCreado = new Usuario(nuevoUsuario);
+    await usuarioCreado.save();
+    return usuarioCreado;
+}
