dfbeaf421626ef32660c68090af09b4a6b5449ca
Magodnes <ropin1697@gmail.com>
2026-04-09 11:22:18 -0300

    Agrego Modelo de Usuario.

---
+++ V1/models/usuario.model.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+const usuarioSchema = new mongoose.Schema({
+    nombre: { type: String, required: true, unique: true },
+    contraseña: { type: String, required: true },
+    plan: { type: String, enum: ['plus', 'premium'], default: 'plus' },
+    fechaCreacion: { type: Date, default: Date.now },
+});
+
+export default mongoose.model('Usuario', usuarioSchema);
