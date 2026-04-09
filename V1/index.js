1cfef3118e8427550f44d722839e20511c60bbed
Magodnes <ropin1697@gmail.com>
2026-04-09 15:22:16 -0300

    Agrega la ruta para gestionar recetas en el enrutador principal. en index.js

--- V1/index.js
+++ V1/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import authRouter from './routes/auth.routes.js';
 import ingredientesRouter from './routes/ingredientes.routes.js';
 import usuariosRouter from './routes/usuarios.routes.js';
+import recetasRouter from './routes/recetas.routes.js';
 import { autorizationMiddleware } from './middlewares/authorization.middleware.js';
 
 const router = express.Router({ mergeParams: true });
@@ -20,5 +21,6 @@ router.use("/ingredientes", ingredientesRouter);
 
 router.use("/usuarios",usuariosRouter);
 
+router.use("/recetas", recetasRouter);
 
 export default router;
