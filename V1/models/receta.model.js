1222d0335e4a8204f032483953260c89526f79a3
Magodnes <ropin1697@gmail.com>
2026-04-09 14:59:07 -0300

    Agrega modelo Receta con esquema para gestionar recetas en la base de datos.

---
+++ V1/models/receta.model.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const recetaSchema = new mongoose.Schema({
+    titulo: { type: String, required: true },
+    descripcion: { type: String, required: true },
+    ingredientes: [{ type: String, required: true }],
+    pasos: [{ type: String, required: true }],
+    autor: { type: String, required: true },
+    fechaDeCreacion: { type: Date, default: Date.now },
+    dificultad: { type: String, enum: ['Fácil', 'Media', 'Difícil'], required: true },
+    
+});
+
+export default mongoose.model("Receta", recetaSchema);
