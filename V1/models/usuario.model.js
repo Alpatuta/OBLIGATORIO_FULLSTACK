b0c924b334e065748393915cf0b4dfb95a360a6f
Magodnes <ropin1697@gmail.com>
2026-04-13 10:23:07 -0300

    Agrego correo en el esquema de modelo usuario para agregar el CORREO como unique.

--- V1/models/usuario.model.js
+++ V1/models/usuario.model.js
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose';
 
 const usuarioSchema = new mongoose.Schema({
-    nombre: { type: String, required: true, unique: true },
+    nombre: { type: String, required: true},
+    correo: { type: String, required: true, unique: true },
     contrasenia: { type: String, required: true },
     plan: { type: String, enum: ['plus', 'premium'], default: 'plus' },
     fechaCreacion: { type: Date, default: Date.now },
