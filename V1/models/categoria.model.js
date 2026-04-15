ea32b8eadc74153faeec7fe2efe4805cd3761b67
FACULTADES\alumnoFI <alumnoFI>
2026-04-15 08:40:03 -0300

    Agrega campo de recetas en el modelo de Categoria para relacionar con el modelo de Receta.

--- V1/models/categoria.model.js
+++ V1/models/categoria.model.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
+import Receta from "./receta.model.js";
 
 const categoriaSchema = new mongoose.Schema({
     nombre: { type: String, required: true, unique: true },
     descripcion: { type: String, required: true },
+    //Agrego un campo de recetas para poder mostrar las recetas que pertenecen a cada categoría. Este campo es un array de ObjectId de MongoDB que hace referencia al modelo de Receta.
+    recetas: [{ type: mongoose.Schema.Types.ObjectId, ref: "Receta" }],
 });
 
 export default mongoose.model("Categoria", categoriaSchema);
