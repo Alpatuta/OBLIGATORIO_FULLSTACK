0cd266b97d49420e86a29fa83ecd88cbfa6f97d1
Alpatuta <fede.oteiza@gmail.com>
2026-04-20 10:24:44 -0300

    Corregir la importación del controlador de subirImagen para incluir la extensión .js

--- V1/routes/uploads.routes.js
+++ V1/routes/uploads.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { subirImagen } from "../controllers/uploads.controller";
+import { subirImagen } from "../controllers/uploads.controller.js";
 
 const router = express.Router({mergeParams: true});
 
