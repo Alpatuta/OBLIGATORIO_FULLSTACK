a85213c086b17567db490932e0ed06ab622f9ed8
Magodnes <ropin1697@gmail.com>
2026-04-22 10:03:28 -0300

    Agregar controlador y rutas para la gestión de categorías: crear, obtener, actualizar y eliminar.

---
+++ V1/routes/categorias.routes.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+import {  } from "../controllers/categorias.controller.js";
+
+const router = express.Router({mergeParams: true});
+
+router.post("/", crearCategoria);
+router.get("/", obtenerCategorias);
+router.get("/:id", obtenerCategoriaPorId);
+router.patch("/:id", actualizarCategoria);
+router.delete("/:id", eliminarCategoria);
+
+export default router;
