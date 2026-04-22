aa8ce0cf97bce37393420bd1971248fb5f4a4c3a
Magodnes <ropin1697@gmail.com>
2026-04-22 09:05:12 -0300

    Agregar modelo y validaciones para administradores

---
+++ V1/validators/admin.validators.js
@@ -0,0 +1,34 @@
+import joi from 'joi';
+
+export const crearAdminSchema = joi.object({
+    nombre: joi.string().min(3).max(100).required().messages({
+        'string.base': 'El nombre debe ser una cadena de texto',
+        'string.empty': 'El nombre no puede estar vacío',
+        'string.min': 'El nombre debe tener al menos 3 caracteres',
+        'string.max': 'El nombre no puede tener más de 100 caracteres',
+        'any.required': 'El nombre es obligatorio',
+    }),
+    contrasenia: joi.string().min(6).max(100).pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/).required().messages({
+        'string.base': 'La contraseña debe ser una cadena de texto',
+        'string.empty': 'La contraseña no puede estar vacía',
+        'string.min': 'La contraseña debe tener al menos 6 caracteres',
+        'string.pattern.base': 'La contraseña debe contener al menos una letra mayúscula, una letra minúscula y un número',
+        'string.max': 'La contraseña no puede tener más de 100 caracteres',
+        'any.required': 'La contraseña es obligatoria',
+    }),
+    plan: joi.string().valid('plus', 'premium').default('plus').messages({
+        'any.only': 'El plan debe ser "plus" o "premium"',
+        'any.default': 'El plan por defecto es "plus"',
+    }),
+    confirmarContrasenia: joi.string().valid(joi.ref('contraseña')).required().messages({
+        'any.only': 'Las contraseñas deben coincidir',
+        'any.required': 'La confirmación de contraseña es obligatoria',
+    }),
+    correo: joi.string().email().required().messages({
+        'string.base': 'El correo debe ser una cadena de texto',
+        'string.empty': 'El correo no puede estar vacío',
+        'string.email': 'El correo debe tener un formato válido',
+        'any.required': 'El correo es obligatorio',
+    }),
+
+});
