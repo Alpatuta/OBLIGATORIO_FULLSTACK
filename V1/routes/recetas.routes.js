86f6ea2ff1419a9a82348bab4040659da376c5c3
foteiza <fede.oteiza@gmail.com>
2026-04-23 19:30:18 -0300

    REFAC: Reorganizar ruta de recetas combinadas en el archivo de rutas

--- V1/routes/recetas.routes.js
+++ V1/routes/recetas.routes.js
@@ -12,9 +12,9 @@ const router = express.Router({ mergeParams: true });
 
 router.post("/", crearReceta);
 router.get("/", obtenerRecetas);
+router.get("/combinadas", obtenerRecetasCombinadas);
 router.get("/:id", obtenerRecetaPorId);
 router.patch("/:id", actualizarReceta);
 router.delete("/:id", eliminarReceta);
-router.get("/combinadas", obtenerRecetasCombinadas);
 
 export default router;
