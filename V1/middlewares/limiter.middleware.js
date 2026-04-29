ca68356f1a7cd3ca61bb43ca48b30434bf273cae
foteiza <fede.oteiza@gmail.com>
2026-04-29 08:19:19 -0300

    Fix: Aumentando request limit a 10000 por IP en rate limiter middleware

--- V1/middlewares/limiter.middleware.js
+++ V1/middlewares/limiter.middleware.js
@@ -3,6 +3,6 @@ import rateLimit from 'express-rate-limit';
 // Limite global de requests, comentar el limiter cuando hagamos las pruebas en postaman para no tener problemas con el limite de requests, luego descomentar para que funcione el limiter.
 export const limiter = rateLimit({
     windowMs: 1 * 60 * 1000, // 1 minuto
-    max: 10, // máximo 10 requests por IP
+    max: 10000, // máximo 10000 requests por IP
     message: 'Demasiadas solicitudes desde esta IP, intenta más tarde',
 });
