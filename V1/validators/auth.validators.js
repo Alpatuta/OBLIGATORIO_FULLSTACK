26eecfeca5825ae75d00f3aa76ba434673cbc242
Magodnes <ropin1697@gmail.com>
2026-06-01 09:08:26 -0300

    FIX: Agregando validaciones para el inicio de sesión en el esquema de autenticación

--- V1/validators/auth.validators.js
+++ V1/validators/auth.validators.js
@@ -35,4 +35,16 @@ export const registerSchema = joi.object({
         'any.only': 'El rol debe ser "admin" o "user"',
         'any.default': 'El rol por defecto es "user"',
     }),
+
+});
+
+export const loginSchema = Joi.object({
+    email: Joi.string().email({ tlds: { allow: false } }).required().messages({
+        "string.empty": "El correo electrónico es obligatorio",
+        "string.email": "El correo electrónico no es válido"
+    }),
+    password: Joi.string().min(6).required().messages({
+        "string.empty": "La contraseña es obligatoria",
+        "string.min": "La contraseña debe tener al menos 6 caracteres"
+    })
 });
