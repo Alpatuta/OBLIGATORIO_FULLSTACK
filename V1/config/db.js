4af9e907f3c9f822991fac0155533229071ebe37
foteiza <fede.oteiza@gmail.com>
2026-06-01 10:11:36 -0300

    Fix: Reordenando el console log para que lo muestre antes de que intente la conexion

--- V1/config/db.js
+++ V1/config/db.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI);
         console.log("MONGODB_URI:", process.env.MONGODB_URI);
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log("Connected to MongoDB Goated");
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
